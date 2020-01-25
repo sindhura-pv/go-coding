39ea1bd9f8387f66307ef219ba1f3cdb5c67b7dc
sindhura <sindhura1107@gmail.com>
2020-01-25 15:26:03 -0500

    reverse vowels

--- reverse-vowels.go
+++ reverse-vowels.go
@@ -1,3 +1,64 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"strings"
+)
+
+func reverseVowels(s string) string {
+
+	var l, r int
+	l = 0
+	r = len(s) - 1
+	stringList := strings.Split(s, "")
+
+	for l < r {
+
+		if !isVowel(stringList[l]) {
+			l++
+		}
+
+		if !isVowel(stringList[r]) {
+			r--
+		}
+
+		if isVowel(stringList[l]) && isVowel(stringList[r]) {
+			temp := stringList[l]
+			stringList[l] = stringList[r]
+			stringList[r] = temp
+			l++
+			r--
+		}
+
+	}
+	return listToString(stringList)
+
+}
+
+func isVowel(s string) bool {
+
+	vowels := [5]string{"a", "e", "i", "o", "u"}
+	for _, char := range vowels {
+		if strings.ToLower(s) == char {
+			return true
+		}
+	}
+
+	return false
+
+}
+
+func listToString(arr []string) string {
+
+	var s string
+	for _, char := range arr {
+		s += string(char)
+	}
+	return s
+}
+
+func main() {
+
+	fmt.Print(reverseVowels("hello"))
+
+}
